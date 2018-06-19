3783a0c95b327d04c43e092264621484751d4da9
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-06-19 13:16:31 +0100

    Update Tester Class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
-import javamasterclass.ArrayType;
 
 public class Main {
 	public static void main(String[] args) {
-		
-		int []x = {1,2,3,4,5,5};
-		
-		double result = ArrayType.getAverage(x);
-		System.out.println(result);
+		System.out.println("");
 	}
 }
