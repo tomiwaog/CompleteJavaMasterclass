fb320473c6c0583185c391b49899cfcd843fca3c
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-06-20 18:48:48 +0100

    Updated Mobile Phone and banking application Renamed Packages Moved packages

---
+++ src/ds/arraylistboxing/arraylist/GroceryList.java
@@ -0,0 +1,82 @@
+package ds.arraylistboxing.arraylist;
+
+import java.util.ArrayList;
+
+class GroceryList {
+	private ArrayList<String> groceryListArr = new ArrayList<String>();
+
+	public ArrayList<String> getGroceryListArr() {
+		return groceryListArr;
+	}
+
+	public void addItem(String item) {
+		// adds new string to array-list
+		groceryListArr.add(item);
+	}
+
+	public void printGroceryList() {
+		System.out.println("You have " + groceryListArr.size()
+				+ " elements in " + getClass().getSimpleName());
+		for (int i = 0; i < groceryListArr.size(); i++)
+			System.out.println((i + 1) + ". " + groceryListArr.get(i));
+	}
+
+	/**
+	 * Assumes the users starts counting from 1. 1st position is 1
+	 * 
+	 * @param pos
+	 *            returns the Item (String)
+	 */
+	public String getItem(int pos) {
+		String getItem = null;
+		if (groceryListArr.size() >= pos) {
+			getItem = groceryListArr.get(pos - 1);
+			System.out.println();
+		}
+		return getItem;
+	}
+
+	public void modifyItem(String currItem, String newItem) {
+		int position = findItem(currItem);
+		if (position >= 0) {
+			modifyItem(position, newItem);
+		} else {
+			System.out.println("Cannot modify invalid entry.");
+		}
+	}
+
+	private void modifyItem(int position, String newItem) {
+		groceryListArr.set(position, newItem);
+		System.out.println(groceryListArr.get(position) + " is replaced with "
+				+ newItem);
+	}
+
+	private int findItem(String searchItem) {
+		return groceryListArr.indexOf(searchItem);
+	}
+
+	public boolean onFile(String searchItem) {
+		return findItem(searchItem) >= 0;
+	}
+
+	private void removeItem(int position) {
+		if (position >= 0) {
+			groceryListArr.remove(position);
+		} else
+			System.out.println("Index out of bound");
+	}
+
+	public void removeItem(String item) {
+		int position = findItem(item);
+		if (position >= 0) {
+			removeItem(position);
+			System.out.println(item + " have been removed.");
+		} else {
+			System.out.println("Item Not found. cannot be removde");
+		}
+	}
+
+	public int getSize() {
+		return groceryListArr.size();
+	}
+}
---
+++ src/ds/arraylistboxing/arraylist/Main.java
@@ -0,0 +1,106 @@
+package ds.arraylistboxing.arraylist;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+	private static Scanner scanner = new Scanner(System.in);
+	private static GroceryList groList = new GroceryList();
+
+	public static void main(String[] args) {
+		/*
+		 * //Instance object GroceryList groList2 = new GroceryList();
+		 * groList2.findItem("test");
+		 */
+		boolean quit = false;
+
+		while (!quit) {
+			printInstruction();
+			System.out.println("Enter a choice from Menu");
+			int choice = scanner.nextInt();
+			scanner.nextLine();
+
+			switch (choice) {
+			case 1:
+				printInstruction();
+				break;
+			case 2:
+				addItem();
+				break;
+			case 3:
+				modifyItem();
+				break;
+			case 4:
+				removeItem();
+				break;
+			case 5:
+				itemSearch();
+				break;
+			case 6:
+				groList.printGroceryList();
+				break;
+			case 7:
+				quit = true;
+				break;
+			}
+		}
+	}
+
+//	private static void processArrayList() {
+//		//How to copy an arrayList
+//		ArrayList<String> newArr = new ArrayList<String>();
+//		newArr.addAll(groList.getGroceryListArr());
+//	
+//		//Another way to copy an arraylist
+//		@SuppressWarnings("unused")
+//		ArrayList<String> anotherNewArr = new ArrayList<String>(groList.getGroceryListArr());
+//	
+//		 //Copying an ArrList into an Array
+//		String [] yetAnotherArr = new String [groList.getGroceryListArr().size()];
+//		yetAnotherArr = groList.getGroceryListArr().toArray(yetAnotherArr);
+//	}
+
+	private static void itemSearch() {
+		System.out.println("Enter the Item to search for: ");
+		String key = scanner.nextLine();
+		if (groList.onFile(key))
+			System.out.println(key + " was FOUND!");
+		else
+			System.out.println(key + " was NOT FOUND!");
+	}
+
+	private static void removeItem() {
+		System.out.println("type item to remove");
+		String item = scanner.nextLine();
+		groList.removeItem(item);
+	}
+
+	private static void modifyItem() {
+		System.out.println("Please enter item to replace");
+		String keyItem = scanner.nextLine();
+		System.out.println("Enter the new Item");
+		String newItem = scanner.nextLine();
+		groList.modifyItem(keyItem, newItem);
+	}
+
+	private static void addItem() {
+		System.out.println("Please enter item to add: ");
+		String item = scanner.nextLine();
+		groList.addItem(item);
+
+	}
+
+	private static void printInstruction() {
+		System.out.println("\nPress ...");
+		System.out.println("\t 1 - To print Instruction");
+		System.out.println("\t 2 - To Add item");
+		System.out.println("\t 3 - To modify Item");
+		System.out.println("\t 4 - removeItem");
+		System.out.println("\t 5 - Search for an Item");
+		System.out.println("\t 6 - Print Grocery list");
+		System.out.println("\t 7 - Quit / Exit");
+		System.out.println("\t ");
+	}
+
+}
