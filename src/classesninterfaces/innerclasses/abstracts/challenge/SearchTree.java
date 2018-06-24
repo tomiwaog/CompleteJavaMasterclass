3439c88df3a3b1fca53195ec1c949ae4de83ecfe
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-06-24 12:29:10 +0100

    Own searchTree

---
+++ src/classesninterfaces/innerclasses/abstracts/challenge/SearchTree.java
@@ -0,0 +1,29 @@
+package classesninterfaces.innerclasses.abstracts.challenge;
+
+public class SearchTree implements NodeList{
+
+	@Override
+	public ListItem getRoot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addItem(ListItem listItem) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeItem(ListItem listItem) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void traverse(ListItem root) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
