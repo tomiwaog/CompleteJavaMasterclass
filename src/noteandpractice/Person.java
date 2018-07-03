e950dbac63f52729055b7ccf72d77c5387864455
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-07-03 22:48:06 +0100

    Testing referencing swap. Object unaffected by swap

---
+++ src/noteandpractice/Person.java
@@ -0,0 +1,18 @@
+package noteandpractice;
+
+public class Person {
+
+	private String name;
+	
+	Person(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+}
