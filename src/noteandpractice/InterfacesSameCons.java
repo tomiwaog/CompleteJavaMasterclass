3ab9fff30fbc5df5e8d4c70e3ccfeb84c9863d50
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-07-19 00:36:49 +0100

    Interfaces and modifiers

--- src/noteandpractice/InterfacesSameCons.java
+++ src/noteandpractice/InterfacesSameCons.java
@@ -1,16 +1,16 @@
 package noteandpractice;
 
 public class InterfacesSameCons implements boy,girl {
-void print(){}
 	
 	public static void main(String[] args) {
 		
 		InterfacesSameCons person = new InterfacesSameCons();
-		person.print(); 
 		
 		//System.out.println(person.gender); //Would be ambiguous call.
 		System.out.println(boy.gender);
 		//System.out.println(boy.gender="Man"); //Wouldn't work as Interfaces variables are [static final]
+		
+		System.out.println(person.maxLength());
 	}
 
 
@@ -19,14 +19,32 @@ void print(){}
 		return 0;
 	}
 
+
+	public int distance() {
+		return 0;
+	}
+
+
+	public int maxLength() {
+		return 5;
+	}
+
 }
 
 interface boy{
 	String gender="Boy";
 	int currPos();
+	
+	static int maxLength(){
+		return 4;
+	}
 }
 
 interface girl{
 	String gender="Boy"; 
-	int currPos(); //Uncomment for understanding.[int currPos(int y); would count as seperate method which would require implementation.
+	//String currPos(); //Uncomment for understanding.[int currPos(int y); would count as seperate method which would require implementation.
+	
+	static int distance(){
+		return 0;
+	}
 }
