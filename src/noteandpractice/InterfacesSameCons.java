0a8b7805d2ea9690dec17960ede307f6da9f0c72
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-07-18 21:59:34 +0100

    Rules for implementing methods from multiple interfaces.

--- src/noteandpractice/InterfacesSameCons.java
+++ src/noteandpractice/InterfacesSameCons.java
@@ -11,14 +11,22 @@ void print(){}
 		//System.out.println(person.gender); //Would be ambiguous call.
 		System.out.println(boy.gender);
 		//System.out.println(boy.gender="Man"); //Wouldn't work as Interfaces variables are [static final]
-		
 	}
+
+
+	@Override
+	public int currPos() {
+		return 0;
+	}
+
 }
 
 interface boy{
 	String gender="Boy";
+	int currPos();
 }
 
 interface girl{
-	String gender="Boy";
+	String gender="Boy"; 
+	int currPos(); //Uncomment for understanding.[int currPos(int y); would count as seperate method which would require implementation.
 }
