1947e3a6c8e6e316a5eb5e1af50dd29713a5f6cc
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-07-29 13:05:48 +0100

    Demonstrating polymorphism with return types and covariant types

--- src/noteandpractice/House.java
+++ src/noteandpractice/House.java
@@ -11,15 +11,39 @@ public class House {
 		House room = new Room();
 		System.out.println(house.printNum());
 		System.out.println(room.printNum());
+		
+		Room realRoom = new Room();
+		Object x = new String("Object");
+		realRoom.resize(x);
 	}
 }
 
-class Room extends House {
+class Room extends House implements Resizable{
 
 	@Override
 	Integer printNum() {
 		// Override of Class with subclass demonstration.
 		return 15;
 	}
+	
+	//In order to override only co-variant return type can be different, covariant type doesnt apply to Paramater list
+	//If this method is commented out, then the base version will run
+	@Override
+	public String resize(Object obj){
+		System.out.println("Object param version of Resize. Return type: String");
+		return null;
+	}
 
+	//is not an overriden method, instead an overloaded one.
+	public String resize(String obj){
+		System.out.println("String version of Resize");
+		return null;
+	}
+}
+
+interface Resizable{
+	public default Object resize(Object obj){
+		System.out.println("Interface | Object param version of Resize. Return type: Object");
+		return null;
+	}
 }
