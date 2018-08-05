a8192f8a9f5b694f4a84c463b3984da97fc19770
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-08-05 21:53:54 +0100

    Finally, Try block without any Catch

--- src/noteandpractice/ExceptionPractice.java
+++ src/noteandpractice/ExceptionPractice.java
@@ -1,21 +1,34 @@
 package noteandpractice;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class ExceptionPractice {
 	public static void main(String[] args) {
+
+		// Demonstrating double return in Try catch block. i.e. return in catch
+		// and finally
+		DualReturn dual = new DualReturn();
+		System.out.println("In main :" + dual.getInt());
+
+		//Try block with Finally only
+		try{
+			System.out.println(4/2);
+		}
+		finally{
+			
+		}
 		String str = "Infinity";
-
 		System.out.println("Char at 5 is " + str.charAt(5));
-
 		try {
 			System.out.println("Char at 15 is " + str.charAt(15));
 		} catch (StringIndexOutOfBoundsException s) {
 			System.out
-					.println("There is no character at that index. index is higher than length-1. | "
+					.println("There is no char at that index. index is higher than length-1. | "
 							+ s);
 		}
 
 		if (str.length() > 3) {
-			System.out.println("about to throw ArrayIndexOutOfBoundsException");
 			try {
 				// Unchecked Exceptions doesn't follow declare-or-handle rule
 				throw new RE();
@@ -27,14 +40,45 @@ public class ExceptionPractice {
 				System.out.println(x);
 			}
 		}
-		System.out.println("Thrown ArrayIndexOutOfBoundsException");
+
+		try {
+			myChEx();
+		} catch (FileNotFoundException e) {
+			System.out.println("Cant find file | " + e);
+			try {
+				throw new NullPointerException();
+			} catch (NullPointerException nuller) {
+				System.out.println("Object is null my friend | " + nuller);
+			}
+		}
 	}
 
+	// Checked Exceptions requires declaration.
+	static void myChEx() throws FileNotFoundException {
+		FileInputStream y = new FileInputStream("");
+		System.out.println("my ChEx method ends!");
+	}
 }
 
 class RE extends ArrayIndexOutOfBoundsException {
-
 	public String toString() {
 		return "RE stands for RuntimeException";
+	}
+}
+
+class DualReturn {
+	int getInt() {
+		int returnVal = 10;
+		try {
+			String[] students = { "Aaa", "Bbb" };
+			System.out.println(students[5]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("about to return " + returnVal);
+			return returnVal;
+		} finally {
+			returnVal +=10;
+			System.out.println("Return value is now "+ returnVal);
+		}
+		return returnVal;
 	}
 }
