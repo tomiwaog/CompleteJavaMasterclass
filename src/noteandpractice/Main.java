0c30057bfb5cc14032f375fe2b70c56aba609aae
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-08-09 20:42:09 +0100

    Assigning String literals to variable on a new line with comment on the assignment line

--- src/noteandpractice/Main.java
+++ src/noteandpractice/Main.java
@@ -7,8 +7,11 @@ package noteandpractice;
 public class Main extends Object {
 
 	public static void main(String[] args) {
+		String literalValueAfterCommentsOnNewLine = //This comment doesn't stop assignment of literal value to String variable
+				"Literal Value assigned after comment";
+		System.out.println(literalValueAfterCommentsOnNewLine);
 		System.out.println("Dividing decimal by Zero : "+23.4/0); //Results in infinity whether negative or positive
-		System.out.println("Dividing decimal by Zero.zero : "+23.4/0.0); //Results in infinity whether negative or positive
+		System.out.println("Dividing decimal by Zero.zero : "+-23.4/0.0); //Results in infinity whether negative or positive
 		// Testing prohibited data type (long) on switch cases
 		// long longNum = 34;
 		// switch(longNum){
