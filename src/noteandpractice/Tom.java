5c5bd39ca6d4912568eac92d9d7ef74ead3b9655
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-10-29 00:06:27 +0000

    Simple Lambda Demo

--- src/noteandpractice/Tom.java
+++ src/noteandpractice/Tom.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tom {
-
 	public static void main(String[] args) {
 		List<Animals> animals = new ArrayList<>();
 		animals.add(new Animals("Snake", false, false));
@@ -13,9 +12,16 @@ public class Tom {
 		animals.add(new Animals("Kangaroo", true, false));
 		animals.add(new Animals("Dolphin", false, true));
 
+		//Utilising 
 		print(animals, new checkHop());
 		System.out.println("Print Swimmable Animals\n");
 		print(animals, new checkSwim());
+		
+		//Lambda Test
+		System.out.println("\nLamda Version: ");
+		print(animals, a -> a.canHop);
+		System.out.println("\nPrint Animals that CANT SWIM:");
+		print(animals, a -> !a.canSwim);
 	}
 
 	static void print(List<Animals> animalList, checkTrait checker) {
@@ -27,25 +33,30 @@ public class Tom {
 	}
 }
 
+//Interface implementation!
 class checkHop implements checkTrait {
 	public boolean test(Animals animal) {
 		return animal.canHop;
 	}
 }
-
-class checkSwim implements checkTrait{
+class checkSwim implements checkTrait {
 	public boolean test(Animals animal) {
 		return animal.canSwim;
 	}
 }
+
 interface checkTrait {
 	boolean test(Animals a);
 }
 
 class Animals {
 	String name;
-	boolean canHop = true;
-	boolean canSwim = true;
+	boolean canHop;
+	boolean canSwim;
+
+	Animals() {
+		this("Unknown Animal", false, false);
+	}
 
 	Animals(String name, boolean hop, boolean swim) {
 		this.name = name;
